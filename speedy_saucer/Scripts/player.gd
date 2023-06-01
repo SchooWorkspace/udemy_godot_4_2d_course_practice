763febc1bfbe063b600bc2f77ab4fe414b449ca8
SchooWorkspace <schoo.workspace@gmail.com>
2023-05-31 22:57:57 -0700

    added movement

--- speedy_saucer/Scripts/player.gd
+++ speedy_saucer/Scripts/player.gd
@@ -1,4 +1,20 @@
 extends RigidBody2D
 
+var speed = 1000
+
 func _ready():
-	apply_impulse(Vector2(20,5))
+	# apply_impulse(Vector2(20,5))
+	pass
+
+func _process(delta):
+	pass
+
+func _physics_process(delta):
+	if Input.is_action_pressed("move_right"):
+		apply_force(Vector2(speed, 0))
+	if Input.is_action_pressed("move_left"):
+		apply_force(Vector2(-speed, 0))
+	if Input.is_action_pressed("move_down"):
+		apply_force(Vector2(0, speed))
+	if Input.is_action_pressed("move_up"):
+		apply_force(Vector2(0, -speed))
