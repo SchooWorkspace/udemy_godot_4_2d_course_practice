5d2a1fba8a487f507c9269884fcf666ee7314c9e
SchooWorkspace <schoo.workspace@gmail.com>
2023-06-01 10:52:37 -0700

    clamping player

--- alien_attack/scripts/player.gd
+++ alien_attack/scripts/player.gd
@@ -1,18 +1,19 @@
 extends CharacterBody2D
 
-var speed = 100
+var speed = 500
 
 func _physics_process(delta):
+	velocity = Vector2(0, 0)
 	if(Input.is_action_pressed("move_right")):
-		velocity = Vector2(speed, 0)
-		move_and_slide()
+		velocity.x = speed
 	if(Input.is_action_pressed("move_left")):
-		velocity = Vector2(-speed, 0)
-		move_and_slide()
+		velocity.x = -speed
 	if(Input.is_action_pressed("move_up")):
-		velocity = Vector2(0, -speed)
-		move_and_slide()
+		velocity.y = -speed
 	if(Input.is_action_pressed("move_down")):
-		velocity = Vector2(0, speed)
-		move_and_slide()
+		velocity.y = speed
 	
+	move_and_slide()
+	
+	global_position.x = clampf(global_position.x, 0, 1280)
+	global_position.y = clampf(global_position.y, 0, 720)
