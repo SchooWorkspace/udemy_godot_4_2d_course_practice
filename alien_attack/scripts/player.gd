f3f0db6b200e46c305889fbb6a84b562e7511692
SchooWorkspace <schoo.workspace@gmail.com>
2023-06-01 19:00:13 -0700

    commit

--- alien_attack/scripts/player.gd
+++ alien_attack/scripts/player.gd
@@ -15,5 +15,16 @@ func _physics_process(delta):
 	
 	move_and_slide()
 	
-	global_position.x = clampf(global_position.x, 0, 1280)
-	global_position.y = clampf(global_position.y, 0, 720)
+#	if(global_position.x < 0):
+#		global_position.x = 0
+#	if(global_position.x > 1280):
+#		global_position.x = 1280
+#	if(global_position.y < 0):
+#		global_position.y = 0
+#	if(global_position.y > 720):
+#		global_position.y = 720
+	
+#	global_position.x = clampf(global_position.x, 0, 1280)
+#	global_position.y = clampf(global_position.y, 0, 720)
+	
+	global_position = global_position.clamp(Vector2(0,0), get_viewport_rect().size)
