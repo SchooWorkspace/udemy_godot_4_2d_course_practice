edda6e8fb977e472e0f6d64c2b08fb6162c34bd9
SchooWorkspace <schoo.workspace@gmail.com>
2023-06-02 12:59:57 -0700

    adding death

--- martian_mike/scripts/level.gd
+++ martian_mike/scripts/level.gd
@@ -1,7 +1,14 @@
 extends Node2D
 
+@onready var start_position = $StartPosition
+
 func _process(delta):
 	if(Input.is_action_just_pressed("quit")):
 		get_tree().quit()
 	elif(Input.is_action_just_pressed("reset")):
 		get_tree().reload_current_scene()
+
+
+func _on_death_zone_body_entered(body):
+	body.velocity = Vector2.ZERO
+	body.global_position = start_position.global_position
