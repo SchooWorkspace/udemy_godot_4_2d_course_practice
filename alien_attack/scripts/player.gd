34e110be86846a55dea878507a6958f52f992e14
SchooWorkspace <schoo.workspace@gmail.com>
2023-06-01 20:53:26 -0700

    rocket shooting function

--- alien_attack/scripts/player.gd
+++ alien_attack/scripts/player.gd
@@ -1,6 +1,14 @@
 extends CharacterBody2D
 
 var speed = 500
+var rscene = preload("res://scenes/rocket.tscn")
+
+func _ready():
+	pass
+
+func _process(delta):
+	if(Input.is_action_just_pressed("shoot")):
+		shoot()
 
 func _physics_process(delta):
 	velocity = Vector2(0, 0)
@@ -12,19 +20,13 @@ func _physics_process(delta):
 		velocity.y = -speed
 	if(Input.is_action_pressed("move_down")):
 		velocity.y = speed
+
 	
 	move_and_slide()
 	
-#	if(global_position.x < 0):
-#		global_position.x = 0
-#	if(global_position.x > 1280):
-#		global_position.x = 1280
-#	if(global_position.y < 0):
-#		global_position.y = 0
-#	if(global_position.y > 720):
-#		global_position.y = 720
-	
-#	global_position.x = clampf(global_position.x, 0, 1280)
-#	global_position.y = clampf(global_position.y, 0, 720)
-	
 	global_position = global_position.clamp(Vector2(0,0), get_viewport_rect().size)
+
+func shoot():
+	var rocket_int = rscene.instantiate()
+	add_child(rocket_int)
+	rocket_int.global_position.x += 50
