afc64388c7c2ee7f4d1d348a9cb2c95d84183431
SchooWorkspace <schoo.workspace@gmail.com>
2023-06-01 22:34:25 -0700

    adding player life ui

--- alien_attack/scripts/game.gd
+++ alien_attack/scripts/game.gd
@@ -8,9 +8,11 @@ var score = 0
 
 func _ready():
 	hud.set_score_label(score)
+	hud.set_lives(lives)
 
 func _on_player_took_damage():
 	lives -= 1
+	hud.set_lives(lives)
 	if(lives < 1):
 		print("Game Over")
 		player.die()
@@ -23,3 +25,4 @@ func _on_enemy_spawner_enemy_spawned(enemy_intance):
 func _on_enemy_died():
 	score += 100
 	hud.set_score_label(score)
+
--- alien_attack/scripts/hud.gd
+++ alien_attack/scripts/hud.gd
@@ -1,6 +1,10 @@
 extends Control
 
 @onready var score = $Score
+@onready var life_left = $LivesLeft
 
 func set_score_label(new_score):
 	score.text = "Score: " + str(new_score)
+
+func set_lives(amount):
+	life_left.text = str(amount)
