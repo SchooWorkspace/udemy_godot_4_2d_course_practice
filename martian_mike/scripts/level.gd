e2e29287917ec93e3ed4865973e29fa9908abbd3
SchooWorkspace <schoo.workspace@gmail.com>
2023-06-02 00:33:52 -0700

    finish project setup

--- martian_mike/scripts/level.gd
+++ martian_mike/scripts/level.gd
@@ -1,2 +1,7 @@
 extends Node2D
 
+func _process(delta):
+	if(Input.is_action_just_pressed("quit")):
+		get_tree().quit()
+	elif(Input.is_action_just_pressed("reset")):
+		get_tree().reload_current_scene()
