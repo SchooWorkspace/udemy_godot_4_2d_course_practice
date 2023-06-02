7116aab04cc6cbc554979e3a7c556ad552fc98f7
SchooWorkspace <schoo.workspace@gmail.com>
2023-06-02 12:46:04 -0700

    adding animations

--- martian_mike/scripts/Player.gd
+++ martian_mike/scripts/Player.gd
@@ -12,10 +12,28 @@ func _physics_process(delta):
 	if !is_on_floor():
 		if velocity.y < max_fall_speed:
 			velocity.y += gravity * delta
-	elif Input.is_action_just_pressed("jump"):
+	if Input.is_action_just_pressed("jump"): # && is_on_floor():
 		velocity.y = -jump_force
 	
 	var direction = Input.get_axis("move_left", "move_right")
 	
+	if direction != 0:
+		animated_sprite.flip_h = (direction == -1)
+	
+	update_animation(direction)
 	velocity.x = direction * speed
 	move_and_slide()
+
+
+func update_animation(direction):
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("run")
+	else:
+		if velocity.y < 0:
+			animated_sprite.play("jump")
+		else:
+			animated_sprite.play("fall")
+	
