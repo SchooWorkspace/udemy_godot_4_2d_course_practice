9002b4f4411f76dbb66318b7920f7b835e981cd5
SchooWorkspace <schoo.workspace@gmail.com>
2023-06-01 21:37:08 -0700

    enhance enemy spawner

--- alien_attack/scripts/enemy_spawner.gd
+++ alien_attack/scripts/enemy_spawner.gd
@@ -1,4 +1,15 @@
 extends Node2D
 
+@onready var enemy_scene = preload("res://scenes/enemy.tscn")
+@onready var spawn_positions = $SpawnPositions
+
 func _on_timer_timeout():
-	print("time out")
+	spawn_enemy()
+
+func spawn_enemy():
+	var positions_array = spawn_positions.get_children()
+	var random_position = positions_array.pick_random()
+	
+	var enemy_instantiation = enemy_scene.instantiate()
+	enemy_instantiation.global_position = random_position.global_position
+	add_child(enemy_instantiation)
