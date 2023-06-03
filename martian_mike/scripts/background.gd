aed73d6fcfae807c3d4aa5ee25276dd305494da6
SchooWorkspace <schoo.workspace@gmail.com>
2023-06-03 09:58:01 -0700

    scrolling background

---
+++ martian_mike/scripts/background.gd
@@ -0,0 +1,16 @@
+extends ParallaxBackground
+
+@export var bg_texture: CompressedTexture2D = preload("res://assets/textures/bg/Blue.png")
+
+@export var scroll_speed = 30
+
+@onready var sprite = $ParallaxLayer/Sprite2D
+
+func _ready():
+	sprite.texture = bg_texture
+
+func _process(delta):
+	sprite.region_rect.position += delta * Vector2(scroll_speed, scroll_speed)
+	if(sprite.region_rect.position >= Vector2(64, 64)):
+		sprite.region_rect.position = Vector2.ZERO
+	print(sprite.region_rect.position)
